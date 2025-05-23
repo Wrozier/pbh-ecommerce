6ab0e601e6bf5b46702f8febc397020d3304b86b
William Rozier <wrozier@princesscruises.com>
2025-05-23 10:44:20 -0400

    Sanity studio connected & Sripe Update

--- sanity/schemaTypes/saleType.ts
+++ sanity/schemaTypes/saleType.ts
@@ -12,6 +12,54 @@ export const saleType = defineType({
             title: 'Sale Title',
             type: 'string',
         }),
+        defineField({
+            name: 'description',
+            title: 'Sale Description',
+            type: 'string',
+        }),
+        defineField({
+            name: 'badge',
+            title: 'Discount Badge',
+            type: 'string',
+            description: 'Discount badge Ratio',
+        }),
+        defineField({
+            name: 'discountAmount',
+            title: 'Discount Amount',
+            type: 'number',
+            description: 'Discount Amount in percentage',
+        }),
+        defineField({
+            name: 'couponCode', 
+            title: 'Coupon Code',
+            type: 'string',
+            description: 'Coupon code for the sale',
+        }),
+        defineField({ 
+            name: 'validFrom',
+            title: 'Valid From',
+            type: 'datetime',
+        }),
+        defineField({
+            name: 'validUntil',
+            title: 'Valid Until',
+            type: 'datetime',
+        }),
+        defineField({
+            name: 'isActive',
+            title: 'Is Active',
+            type: 'boolean',
+            description: 'Is the sale active/not active?',
+            initialValue: true,
+        }),
+        defineField({
+            name: 'image',
+            title: 'Product',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        }),
     ],
 
 })
