9a271aca5347ce301759119a96277751d8a83bdf
William Rozier <wrozier@princesscruises.com>
2025-05-25 10:25:26 -0400

    file reconstruction

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+import localFont from "next/font/local";
+
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      localFont: {
+        poppins: ["Poppins"],
+      },
+      colors: {
+        lightOrange: "#fca99b",
+        lightBlue: "#7688db",
+        darkBlue: "#6c7fd8",
+        darkText: "#686e7d",
+        lightBg: "#f8f8fb",
+        background: "hsl(var(--background))", // Define bg-background
+        foreground: "hsl(var(--foreground))", // Define text-foreground
+        border: "hsl(var(--border))", // Define border-border
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
